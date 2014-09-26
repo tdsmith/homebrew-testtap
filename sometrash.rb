646dfaad377efffc4989a24a260d7c8bc0fdec07
Tim D. Smith <git@tim-smith.us>
2014-09-26 11:41:46 -0700

    sometrash: install a resource to delete in test

--- sometrash.rb
+++ sometrash.rb
@@ -5,6 +5,11 @@ class Sometrash < Formula
   url 'https://github.com/ali-rantakari/trash/archive/v0.8.4.tar.gz'
   sha1 'e70ebeba684fd1fd126d912e3528115fbb2fb7be'
 
+  resource "emoji" do
+    url "https://gist.githubusercontent.com/tdsmith/e8b46f5a7ce49b63fb7f/raw/2ca68554549ff98e1109414c0f2623b3a5c81e5f/utf16-emoji.Md"
+    sha1 "24cb9e418d1db2a05ce07ce9383ada9d360e93c3"
+  end
+
   conflicts_with 'osxutils', :because => 'both install a trash binary'
 
   def install
@@ -15,6 +20,8 @@ class Sometrash < Formula
   end
 
   test do
-    system "#{bin}/trash"
+    resource("emoji").stage(testpath)
+    system "cat", "utf16-emoji.Md"
+    system "#{bin}/trash", "utf16-emoji.Md"
   end
 end
